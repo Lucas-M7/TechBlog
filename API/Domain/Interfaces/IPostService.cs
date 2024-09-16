b63be49b3f3f60a9556840e8b5ef71315a5dea73
Lucas Mateus <Lucas015x@gmail.com>
2024-09-16 15:58:09 -0300

    feat: Improvements such as xml comments and removing public from interfaces

--- API/Domain/Interfaces/IPostService.cs
+++ API/Domain/Interfaces/IPostService.cs
@@ -2,10 +2,37 @@ using API.Domain.Models.Post;
 
 namespace API.Domain.Interfaces;
 
+/// <summary>
+/// Interface for post service operations.
+/// </summary>
 public interface IPostService
 {
-    public Task CreatePost(PostModel post);
-    public Task UpdatePost(PostModel post);
-    public Task DeletePost(PostModel post);
-    public Task UpdateUsernameInPosts(string userId, string newUsername);
+    /// <summary>
+    /// Creates a new post.
+    /// </summary>
+    /// <param name="post">The post to create.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task CreatePost(PostModel post);
+
+    /// <summary>
+    /// Updates an existing post.
+    /// </summary>
+    /// <param name="post">The post to update.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task UpdatePost(PostModel post);
+
+    /// <summary>
+    /// Deletes an existing post.
+    /// </summary>
+    /// <param name="post">The post to delete.</param>
+    /// <returns>A task that represent the asynchronous operation.</returns>
+    Task DeletePost(PostModel post);
+
+    /// <summary>
+    /// Updates the username in posts for a specific user.
+    /// </summary>
+    /// <param name="userId">The ID of the user.</param>
+    /// <param name="newUsername">The new username to update.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task UpdateUsernameInPosts(string userId, string newUsername);
 }
--- API/Domain/Interfaces/IUserService.cs
+++ API/Domain/Interfaces/IUserService.cs
@@ -2,8 +2,22 @@ using API.Domain.DTOs;
 
 namespace API.Domain.Interfaces;
 
+/// <summary>
+/// Interface for user service operations.
+/// </summary>
 public interface IUserService
 {
-    public Task Register(RegisterDTO registerDTO);
-    public Task Login(LoginDTO loginDTO);   
+    /// <summary>
+    /// Register a new user.
+    /// </summary>
+    /// <param name="registerDTO">The registe to create.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task Register(RegisterDTO registerDTO);
+
+    /// <summary>
+    /// Login a the user.
+    /// </summary>
+    /// <param name="loginDTO">The login of user.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task Login(LoginDTO loginDTO);
 }
