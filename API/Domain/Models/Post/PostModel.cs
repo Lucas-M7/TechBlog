8179a70fd3e3c5cfef39a49a06c0fe2be56471b7
Lucas Mateus <Lucas015x@gmail.com>
2024-09-17 19:20:56 -0300

    feat: Refactoring model code, adding xml comments and validations

--- API/Domain/Models/Post/PostModel.cs
+++ API/Domain/Models/Post/PostModel.cs
@@ -3,19 +3,43 @@ using Microsoft.AspNetCore.Identity;
 
 namespace API.Domain.Models.Post;
 
+/// <summary>
+/// Model for the post.
+/// </summary>
 public class PostModel
 {
+    /// <summary>
+    /// Gets or sets the post ID.
+    /// </summary>
     [Key]
     public int PostId { get; set; }
 
-    [Required(ErrorMessage = "Title is required."), 
-        MinLength(4, ErrorMessage = "The title must have 4 characters long.")]
+    /// <summary>
+    /// Gets or sets the title of the post.
+    /// </summary>
+    [Required(ErrorMessage = "Title is required.")]
+    [MinLength(4, ErrorMessage = "The title must have at least 4 characters.")]
     public string Title { get; set; } = string.Empty;
 
-    [Required, MinLength(10, ErrorMessage = "The content must have 10 characters long.")]
+    /// <summary>
+    /// Gets or sets the content of the post.
+    /// </summary>
+    [Required(ErrorMessage = "Content is required.")] 
+    [MinLength(10, ErrorMessage = "The content must have at least 10 characters.")]
     public string Content { get; set; } = string.Empty;
 
+    /// <summary>
+    /// Gets or sets the user ID associated with the post.
+    /// </summary>
     public string UserId { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Gets or sets the username associated with the post.
+    /// </summary>
     public string Username { get; set; } = string.Empty;
-    public virtual IdentityUser User { get; set; } = default!;
+
+    /// <summary>
+    /// Gets or sets the user associated with the post.
+    /// </summary>
+    public virtual IdentityUser User { get; set; } = new IdentityUser();
 }
--- API/Domain/Models/User/UserModel.cs
+++ API/Domain/Models/User/UserModel.cs
@@ -1,9 +1,16 @@
-using API.Domain.Models.Post;
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Identity;
 
 namespace API.Domain.Models.User;
 
+/// <summary>
+/// Model for a user.
+/// </summary>
 public class UserModel : IdentityUser
 {
+    /// <summary>
+    /// Gets or sets the age of the user.
+    /// </summary>
+    [Range(0, 120, ErrorMessage = "The age must be between 0 and 120.")]
     public int Age { get; set; }
 }
