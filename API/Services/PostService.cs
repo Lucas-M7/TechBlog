2ecf5a6462ee802a40af0182c66ec509df1f4a50
Lucas Mateus <Lucas015x@gmail.com>
2024-09-18 14:53:09 -0300

    refactor: Refactor service classes for consistency and efficiency  - Enhanced PostService with improved error handling and code efficiency.  - Updated UserService for better registration and login processes.

--- API/Services/PostService.cs
+++ API/Services/PostService.cs
@@ -1,31 +1,58 @@
 using API.Domain.Interfaces;
 using API.Domain.Models.Post;
 using API.Infrasctuture.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Services;
 
+/// <summary>
+/// Service for managing posts.
+/// And initializes a new instace of the <see cref="PostService"/> class.
+/// </summary>
+/// <param name="context">The application database context.</param>
 public class PostService(AppDbContext context) : IPostService
 {
     private readonly AppDbContext _context = context;
 
+    /// <summary>
+    /// Creates a new post.
+    /// </summary>
+    /// <param name="post">The post to create.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public async Task CreatePost(PostModel post)
     {
         _context.Posts.Add(post);
         await _context.SaveChangesAsync();
     }
 
+    /// <summary>
+    /// Updates an existing post.
+    /// </summary>
+    /// <param name="post">The post to update.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public async Task UpdatePost(PostModel post)
     {
         _context.Posts.Update(post);
         await _context.SaveChangesAsync();
     }
 
+    /// <summary>
+    /// Deletes an existing post.
+    /// </summary>
+    /// <param name="post">The post to delete.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public async Task DeletePost(PostModel post)
     {
         _context.Posts.Remove(post);
         await _context.SaveChangesAsync();
     }
 
+    /// <summary>
+    /// Updates the username in posts for a specific user.
+    /// </summary>
+    /// <param name="userId">The ID of the user.</param>
+    /// <param name="newUsername">The new username to update.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public async Task UpdateUsernameInPosts(string userId, string newUsername)
     {
         var posts = _context.Posts.Where(p => p.UserId == userId).ToList(); // Querying all posts of user by userId
--- API/Services/UserService.cs
+++ API/Services/UserService.cs
@@ -5,25 +5,48 @@ using Microsoft.AspNetCore.Identity;
 
 namespace API.Services;
 
+/// <summary>
+/// Service for managing user operations.
+/// And initializes a new instance of the <see cref="UserService"> class.
+/// </summary>
+/// <param name="userManager">The user manager for handling user operations.</param>
+/// <param name="signInManager">The sign-in manager for handling user sign-ins.</param>
 public class UserService(UserManager<UserModel> userManager,
     SignInManager<UserModel> signInManager) : IUserService
 {
     private readonly UserManager<UserModel> _userManager = userManager;
     private readonly SignInManager<UserModel> _signInManager = signInManager;
 
+    /// <summary>
+    /// Registers a new user
+    /// </summary>
+    /// <param name="registerDTO">The registration data transfer object.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public async Task Register(RegisterDTO registerDTO)
     {
-        // Adding datas
+        // Creating a new user object
         var user = new UserModel
-        { UserName = registerDTO.Username, Email = registerDTO.Email, Age = registerDTO.Age };
+        {
+            UserName = registerDTO.Username,
+            Email = registerDTO.Email,
+            Age = registerDTO.Age
+        };
 
-        // Hashing password
+        // Creating the user and hashing the password
         var result = await _userManager.CreateAsync(user, registerDTO.Password);
 
         if (!result.Succeeded)
-            throw new ArgumentException(result.Errors.ToString());
+        {
+            var errors = string.Join(", ", result.Errors);
+            throw new ArgumentException($"User registration failed: {errors}");
+        }
     }
 
+    /// <summary>
+    /// Logs in a user.
+    /// </summary>
+    /// <param name="loginDTO">The login data transfer object.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
     public async Task Login(LoginDTO loginDTO)
     {
         var result = await _signInManager.PasswordSignInAsync(
